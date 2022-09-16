c666313a197377cf14e8d01ea62b5c6cd7e12e80
Cihat Salik <cihatsalik1@gmail.com>
2022-09-17 01:51:01 +0300

    Add not found page to go when wrong url is entered

---
+++ frontend/src/views/NotFound404.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+a-result(status='404' title='404' sub-title='Sorry, the page you visited does not exist.')
+  template(#extra='')
+    a-button(type='primary')
+      router-link(to='/')
+        | Back Home
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
