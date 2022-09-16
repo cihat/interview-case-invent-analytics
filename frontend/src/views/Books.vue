6dfce6ed6e359778eb9795a145d72e6577bb0481
Cihat Salik <cihatsalik1@gmail.com>
2022-09-17 01:46:58 +0300

    Add table component for list all users and books

--- frontend/src/views/Books.vue
+++ frontend/src/views/Books.vue
@@ -1,9 +1,58 @@
 <template lang="pug">
 .container
-  h1 Books Page
+  h1 Books
+  a-table(:columns="columns" :data-source="books" :row-key="record => record._id" :pagination="{ pageSize: 10 }")
+    template(#title='books')
+      a-button(type='primary') Add Book
+    a-button(type="danger" slot='delete-action' slot-scope='text') Delete
+    a-button(type="primary" slot='edit-action' slot-scope='text') Edit
+    book-item
 </template>
 <script>
+import { mapState } from 'vuex'
+import BookItem from '../components/BookItem.vue';
+
+const columns = [
+  {
+    title: "Book Title",
+    dataIndex: "title",
+    key: "title",
+  },
+  {
+    title: "Author",
+    dataIndex: "author",
+    key: "author",
+  },
+  {
+    title: "ISBN",
+    dataIndex: "isbn",
+    key: "isbn",
+  },
+  {
+    title: "Delete",
+    key: "x",
+    scopedSlots: { customRender: "delete-action" },
+  },
+  {
+    title: "Edit",
+    key: "x",
+    scopedSlots: { customRender: "edit-action" },
+  }
+];
+
 export default {
+  name: 'books',
+  component: {
+    BookItem
+  },
+  computed: {
+    ...mapState('book', ['books'])
+  },
+  data() {
+    return {
+      columns
+    };
+  },
 
 }
 </script>
--- frontend/src/views/Users.vue
+++ frontend/src/views/Users.vue
@@ -1,10 +1,52 @@
 <template lang="pug">
-.container
-  h1 Users Page
-</template>
+  .container
+    h1 Users
+    a-table(:columns="columns" :data-source="users" :row-key="record => record._id" :pagination="{ pageSize: 10 }")
+      a-button(type="danger" slot='delete-action' slot-scope='text') Delete
+      a-button(type="primary" slot='edit-action' slot-scope='text') Edit
+      book-item
+  </template>
 <script>
-export default {
+import { mapState } from 'vuex'
 
+const columns = [
+  {
+    title: "User Name",
+    dataIndex: "username",
+    key: "username",
+  },
+  {
+    title: "Email",
+    dataIndex: "email",
+    key: "email",
+  },
+  {
+    title: 'name',
+    dataIndex: 'name',
+    key: 'name'
+  },
+  {
+    title: "Borrow Books",
+    dataIndex: "borrowedBooks",
+    key: "borrowedBooks",
+  },
+  {
+    title: "Received Books",
+    dataIndex: "receivedBooks",
+    key: "receivedBooks",
+  },
+]
+
+export default {
+  name: "users",
+  computed: {
+    ...mapState("account", ["users"]),
+  },
+  data() {
+    return {
+      columns,
+    };
+  },
 }
 </script>
 <style lang="scss">
