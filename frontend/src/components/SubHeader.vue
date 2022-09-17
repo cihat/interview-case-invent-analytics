4ca100365f16c0a84589608b8307266a764cfa23
Cihat Salik <cihatsalik1@gmail.com>
2022-09-17 11:19:09 +0300

    Add profile view and sub header component

---
+++ frontend/src/components/SubHeader.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+a-page-header(style='border: 1px solid rgb(235, 237, 240)' :title='title' :sub-title='subTitle' @back='() => $router.go(-1)')
+</template>
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    subTitle: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+<style lang="scss">
+
+</style>
---
+++ frontend/src/views/Profile.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+.container
+  SubHeader(:title='user.name' :sub-title='user.username')
+</template>
+<script>
+import { mapState } from 'vuex'
+import SubHeader from "../components/SubHeader";
+
+export default {
+  name: 'profile',
+  data() {
+    return {};
+  },
+  components: {
+    SubHeader
+  },
+  computed: {
+    ...mapState('account', ['user'])
+  }
+
+
+}
+</script>
+<style lang="scss">
+
+</style>
